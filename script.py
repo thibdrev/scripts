456f7b4154ca3346a4e16aeb0ac011c161d9b28c
thibdrev <thibault.drevet@gmail.com>
2016-02-16 10:38:06 +1100

    Delete script.py

--- script.py
+++
@@ -1,7 +0,0 @@
-## license WTFPL
-## https://en.wikipedia.org/wiki/WTFPL
-
-from bs4 import BeautifulSoup
-
-URL = "http://www.education.gouv.fr/pid24302/annuaire-resultat-recherche.html?lycee_name="
-html = BeautifulSoup(URL,'lxml')
