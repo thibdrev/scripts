c3ebd36f31bf869fdb25c6e30c1fc0c696f5f6bc
thibdrev <thibault.drevet@gmail.com>
2016-02-16 10:10:48 +1100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
-http://www.education.gouv.fr/pid24302/annuaire-resultat-recherche.html?lycee_name=%v%
+## license WTFPL
+## https://en.wikipedia.org/wiki/WTFPL
+
+from bs4 import BeautifulSoup
+
+URL = "http://www.education.gouv.fr/pid24302/annuaire-resultat-recherche.html?lycee_name="
+html = BeautifulSoup(URL,'lxml')
